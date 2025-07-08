699d68a1519ee9ca2898aa83c346b7dc3e62a729
capprinha <gutoklein21@gmail.com>
2025-07-08 20:09:58 -0300

    componetizando a lista de tarefas \ enums

--- src/containers/LIstaDeTarefas/index.tsx
+++ src/containers/LIstaDeTarefas/index.tsx
@@ -1,7 +1,40 @@
+import Tarefa from "../../components/Tarefa"
+import { Container } from "./styles"
+
+import * as enums from '../../utils/enunms/Tarefa'
+
+const tarefas = [
+  {
+    titulo: 'Estudar TypeScript',
+    descricao: 'Ver a aula 3 da EBAC',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  },
+  {
+    titulo: 'Pagar conta de internet',
+    descricao: 'Ver fatura no e-mail',
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
+  },
+  {
+    titulo: 'Ir pra academia',
+    descricao: 'Fazer treino pesado',
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
+  <Container>
     <p>2 tarefas marcadas como: "categoria" e "terno"</p>
-  </main>
+    <ul>
+      {tarefas.map(t =>
+      <li key={t.titulo}>
+        <Tarefa descricao={t.descricao} status={t.status} titulo={t.titulo} prioridade={t.prioridade}/>
+      </li>
+      )}
+    </ul>
+  </Container>
 )
 
 export default ListaDeTarefas
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -6,6 +6,7 @@ const EstiloGlobal = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-family: Roboto, sans-serif;
+    list-style: none;
   }
 `
 
